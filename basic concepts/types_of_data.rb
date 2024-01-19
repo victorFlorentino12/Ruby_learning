ddd900c177e6954a96847695fb26cf85d3022c0c
victorFlorentino12 <140027327+victorFlorentino12@users.noreply.github.com>
2024-01-19 10:51:25 -0300

    Create types_of_data.rb

---
+++ basic concepts/types_of_data.rb
@@ -0,0 +1,19 @@
+number_int = -20
+
+number_int.class
+
+number_float = 30.3
+
+number_float.class
+
+boolean = true
+
+boolean.class
+
+string = "i'm a words"
+
+string.class
+
+array = [1,3,5,"string"]
+
+symbol = :ruby_symbol
